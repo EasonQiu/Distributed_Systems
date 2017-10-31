fb414fbf7e66d6e777465d42fe8b056b67c95a6e
Eason Qiu <gdjd94@qq.com>
2017-10-31 16:11:16 -0400

    init

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 10/12/17 10:09 PM
+# @Author  : Ethan
+# @Site    :
+# @File    : config.py
+# @Software: PyCharm
+
+
+state_file_path = './logs/state.txt'
+log_file_path = './logs/twitter.txt'
+partial_log_file_path = './logs/partial_log.txt'
+id_ports = 'ips.txt'
+blocking_info_file_path = './logs/blocking_dict.txt'
+
+peers = []
